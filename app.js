a55238b4438a6273d3641a3f1a754747621945b0
Vishalmork <112015513+VishalGovathoti@users.noreply.github.com>
2022-11-01 19:42:05 -0500

    added new files

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var selectorRouter = require('./routes/selector')
+var carRouter = require('./routes/car');
+var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -22,8 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/car', carRouter);
+app.use('/gridbuild', gridbuildRouter);
 app.use('/selector', selectorRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
